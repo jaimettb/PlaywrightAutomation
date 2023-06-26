fc03e417051900c8abf2a1fc439a739f6a79112e
Jaime <jaimettb@gmail.com>
2023-06-26 18:00:12 -0300

    Fixed base URL

--- src/tests/api/api.spec.ts
+++ src/tests/api/api.spec.ts
@@ -1,15 +1,28 @@
 import {test, expect} from '@playwright/test'
 
-test.describe.parallel.only("API Testing", ()=>{
-    const baseUrl = "http:regres.in/api";
+test.describe.parallel("API Testing", ()=>{
+    const baseUrl = "http:reqres.in/api";
 
     test("Simple API Test - Assert Response Status",async ({request}) => {
-        const response = await request.get(`${baseUrl}/users/2`);
+        const response = await request.get(`${baseUrl}/users/3`);
         expect(response.status()).toBe(200);
+
+        const responseBody = JSON.parse(await response.text());
     })
 
     test("Simple API Test - Assert Invalid Endpoint",async ({request}) => {
-        const response = await request.get(`https://oslogin.googleapis.com/v1/%7Bname=users/*%7D/loginProfile`);
+        const response = await request.get(`${baseUrl}/users/2/fdffds`);
         expect(response.status()).toBe(404);
+    })
+
+    test("GET Request - Get User Detail", async ({request})=>{
+        const response = await request.get(`${baseUrl}/users/1`)
+        const responseBody = JSON.parse(await response.text());
+
+        expect(response.status()).toBe(200);
+        expect(responseBody.data.id).toBe(1);
+        expect(responseBody.data.first_name).toBe("George");
+        expect(responseBody.data.last_name).toBe("Bluth");
+        expect(responseBody.data.email).toBeTruthy();
     })
 })
