60a04ccca3b38b512b3210dd4943e6fbcad9cf67
Jaime <jaimettb@gmail.com>
2023-06-29 08:50:39 -0300

    Skip a specific browser

--- src/tests/tips/tips.spec.ts
+++ src/tests/tips/tips.spec.ts
@@ -4,5 +4,11 @@ test.describe.only('Tips & Tricks Section', ()=>{
     test('TestInfo Object', async({page}, testInfo) => {
         await page.goto('https://www.example.com');
         console.log(testInfo.expectedStatus);
+    });
+
+    test('Test Skip Browser', async({page, browserName}) => {
+        console.log(browserName);
+        test.skip(browserName === "chromium", "Feature not ready in Chrome borwser");
+        await page.goto('https://www.example.com');
     })
 })
