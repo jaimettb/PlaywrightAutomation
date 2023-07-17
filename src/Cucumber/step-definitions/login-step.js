fc8574fb9e374189695fccebcfb6fc7208c6371b
Jaime <jaimettb@gmail.com>
2023-07-17 16:04:34 -0300

    Finalized Cucumber tests

--- src/Cucumber/step-definitions/login-step.js
+++ src/Cucumber/step-definitions/login-step.js
@@ -1,5 +1,24 @@
 const { Given, When, Then, defineStep } = require("@cucumber/cucumber");
+const {LoginPage} = require("../page-objects/login-page");
+
+const loginPage = new LoginPage();
 
 Given("I visit a login page", async function (){
-    await page.goto('https://www.saucedemo.com/')
+    await loginPage.navigateToLoginScreen();
+})
+
+When("I fill the login form with valid credentials", async function(){
+    await loginPage.submitLoginForm();
+})
+
+When("I should see the home page", async function(){
+    await loginPage.assertUserIsLoggedIn();
+})
+
+defineStep('I wait for 3 seconds', async function(){
+    await loginPage.pause();
+})
+
+defineStep(/^I fill the login form with "([^"]*)" and "([^"]*)"$/, async function(username, password){
+    await loginPage.submitLoginWithParameters(username, password);
 })
