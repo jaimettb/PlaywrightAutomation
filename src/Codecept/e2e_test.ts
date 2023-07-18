63fc6023553a998c9fb484c2460daecb82b4d456
Jaime <jaimettb@gmail.com>
2023-07-18 08:29:07 -0300

    Added Codecept project

---
+++ src/Codecept/e2e_test.ts
@@ -0,0 +1,19 @@
+Feature("Zero Bank Application - E2E Tests");
+
+Before(({I})=>{
+    console.log('BEFORE HOOK');   
+    I.amOnPage('http://zero.webappsecurity.com/index.html');
+})
+
+After(({I})=>{
+    console.log('AFTER HOOK');   
+})
+
+Scenario("Login Test - Negative", ({I})=>{
+    I.click('#signin_button');
+    I.seeElement("#login_form");
+    I.fillField("#user_login", "invalid username");
+    I.fillField("#user_password", "invalid password");
+    I.click('.btn-primary');
+    I.seeElement(".alert-error");
+})
